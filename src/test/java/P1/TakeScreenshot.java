16d28a3c49b2fc1855fe284b744f5a94888d9a28
KishorAutomation <86106904+KishorAutomation@users.noreply.github.com>
2021-07-12 15:47:46 +0530

    Create TakeScreenshot.java

---
+++ src/test/java/P1/TakeScreenshot.java
@@ -0,0 +1,31 @@
+package P1;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+public class TakeScreenshot {
+	WebDriver driver;
+	
+	@Test
+	public void Screenshot() throws InterruptedException, IOException{
+		System.setProperty("webdriver.chrome.driver", "F:\\Automation files\\SPRAOI\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://online.actitime.com/ky");
+		driver.manage().window().fullscreen();
+		Thread.sleep(10000);
+		Screenshot s=new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+        ImageIO.write(s.getImage(),"PNG",new File("F:\\Automation files\\SPRAOI\\screenshots\\fullPageScreenshot3.png"));
+        driver.close();
+		
+	}
+	}
+
