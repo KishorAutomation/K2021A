42a4707001e7088d811def83a6694565669c9a33
KishorAutomation <86106904+KishorAutomation@users.noreply.github.com>
2021-07-12 15:24:05 +0530

    Add files via upload

---
+++ K2021A/src/main/java/K2021A/K2021A/App.java
@@ -0,0 +1,13 @@
+package K2021A.K2021A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
